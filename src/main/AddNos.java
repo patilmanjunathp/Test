055cf361f03316ecb82b2f25e22c1db3bede2363
patilmanjunathp <patilmanjunathp@gmail.com>
2022-03-22 20:57:00 +0530

    First commit

---
+++ src/main/AddNos.java
@@ -0,0 +1,27 @@
+package main;
+
+import model.Multiflication;
+
+public class AddNos {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		int n1=2;
+		int n2=3;
+		int n3;
+		n3=n1+n2;
+		
+		System.out.println(n3);
+		
+		Multiflication s1=new Multiflication();
+		
+	s1.a=5;
+	s1.b=6;
+	
+System.out.println(s1.mulTwoNos());
+	
+	}
+
+}
---
+++ src/model/Multiflication.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Multiflication {
+	
+	public int a;
+	public int b;
+	
+	public int mulTwoNos()
+	{
+		int c =a*b;
+		
+		return c;
+	}
+	
+
+}
